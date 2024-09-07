ed25c52a003c98a549d98077786d5ff8587f8be9
Ulrich HOUNGBE <zenithdeo21@proton.me>
2024-09-08 00:35:25 +0100

    modif du app , verification build ulrich

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Simuler une base de données d'utilisateurs
+# Commentaire ajouter pour vérifier le build pour une autre branche
 users = [
     {"id": 1, "name": "Alice", "email": "alice@example.com"},
     {"id": 2, "name": "Bob", "email": "bob@example.com"}
