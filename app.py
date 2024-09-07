65997f0c59425ac78a5aa5c88de02c0e26014a0b
Ulrich HOUNGBE <zenithdeo21@proton.me>
2024-09-07 22:47:21 +0100

    build

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Simuler une base de données d'utilisateurs
+users = [
+    {"id": 1, "name": "Alice", "email": "alice@example.com"},
+    {"id": 2, "name": "Bob", "email": "bob@example.com"}
+]
+
+@app.route('/')
+def home():
+    return "Bienvenue sur l'API Utilisateurs!"
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
